c54fc3a39f94cb9ab2c13f2ea15f170bd1396730
Unfamous Dyson <dougdyson@gmail.com>
2022-02-16 01:22:49 -0800

    clean code

--- src/components/FormsUI/Selector/Selector.jsx
+++ src/components/FormsUI/Selector/Selector.jsx
@@ -7,23 +7,27 @@ const SelectWrapper = ({
   options,
   ...otherProps
 }) => {
+  // form state
   const { setFieldValue } = useFormikContext();
   const [field, meta] = useField(name);
 
+  // user change
   const handleChange = evt => {
     const { value } = evt.target;
     setFieldValue(name, value);
   };
 
+  // set values
   const configSelect = {
     ...field,
     ...otherProps,
+    fullWidth: true,
     select: true,
     variant: 'outlined',
-    fullWidth: true,
     onChange: handleChange
   };
 
+  // user error
   if (meta && meta.touched && meta.error) {
     configSelect.error = true;
     configSelect.helperText = meta.error;
