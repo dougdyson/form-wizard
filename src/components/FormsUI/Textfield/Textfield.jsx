6e25f03affd17db94d92c1ad78492090cb905f38
Unfamous Dyson <dougdyson@gmail.com>
2022-02-15 22:31:02 -0800

    update component

--- src/components/FormsUI/Textfield/Textfield.jsx
+++ src/components/FormsUI/Textfield/Textfield.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import { TextField } from "@mui/material";
+import { useField } from "formik";
 
-const TextfieldWrapper = () => {
+
+const TextfieldWrapper = ({name, ...otherProps}) => {
+  // manafor form state
+  const [field, info] = useField(name);
+  
+  // set style defaults
+  const configTextfield = {
+    ...field,
+    ...otherProps,
+    variant: 'outlined',
+    fullWidth: true
+  }
+
+  if (meta && meta.touched && meta.error){
+    configTextfield.error = true;
+    configTextfield.helperText = meta.error;
+  }
+
   return (
-    <TextField />
+    <TextField {...configTextfield} />
   )
-}
+};
 
 export default TextfieldWrapper;
