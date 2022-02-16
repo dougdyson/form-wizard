073de3f11618d78cb44223bc2b44c9e8e4831ec5
Unfamous Dyson <dougdyson@gmail.com>
2022-02-15 19:35:36 -0800

    draft out page layout

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,28 @@
 import { makeStyle } from '@material-ui/core/styles';
-import { Container } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
+import { 
+  Container,
+  Grid
+} from '@material-ui/core';
+
 
 function App() {
   return (
     <Container>
-      <div>hello</div>
+      <Grid container direction="row" justifyContent="space-between" alignItems="center">
+        <Grid item>
+          <Typography>Logo</Typography>
+        </Grid>
+        <Grid item>
+          <Typography>Account</Typography>
+        </Grid>
+      </Grid>
+      <Typography>Entity/Compliance List</Typography>
+      <Grid container>
+        <Grid xs="12">Item in list</Grid>
+        <Grid>Item in list</Grid>
+      </Grid>
     </Container>
   );
 }
