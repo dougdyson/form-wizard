67c465ea1e2dca5e7d2de9e35bd79c010d093632
Unfamous Dyson <dougdyson@gmail.com>
2022-02-15 22:18:07 -0800

    add textfield component

---
+++ src/components/FormsUI/Textfield/Textfield.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { TextField } from "@mui/material";
+
+const TextfieldWrapper = () => {
+  return (
+    <TextField />
+  )
+}
+
+export default TextfieldWrapper;
