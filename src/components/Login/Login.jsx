7067ec05a91890820cf45a7539c5d7f47d18e40b
Unfamous Dyson <dougdyson@gmail.com>
2022-02-16 16:58:08 -0800

    clean up code

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -4,12 +4,8 @@ import { TextField, Button, Typography, Grid } from "@mui/material";
 
 const Login = () => {
   return (
-    <Grid
-    container
-    direction="column"
-    justifyContent="center"
-    alignItems="center"
-  >
+    <Grid container direction="column" justifyContent="center" alignItems="center"
+    >
       <Typography variant='overline'>Sign In</Typography>
       <TextField margin="normal" size="small" id="outlined-basic" label="email" variant="outlined" />
       <TextField size="small" id="outlined-basic" label="password" variant="outlined" />
