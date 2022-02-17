1cb7faa7b9bcb2a6df25d575afe5e9c6519072d5
Unfamous Dyson <dougdyson@gmail.com>
2022-02-16 22:57:05 -0800

    add unstyled login component skeleton

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -7,35 +7,36 @@ export default function Login() {
   
   return (
     <Grid 
-    container 
-    direction="column" 
-    justifyContent="center" 
-    alignItems="center">
+      container 
+      direction="column" 
+      alignItems="center">
       <form noValidate autoComplete="off">
         <Grid>
           <TextField 
             id="email"
             label="email"
             variant="outlined"
-            required
-            size="small" 
-            margin="normal" 
-            size="small" 
-            id="outlined-basic" />
+            margin="normal"
+            size="small"
+            gutterBottom="true"
+          />
         </Grid>
         <Grid>
           <TextField 
             id="password"
             label="password" 
             variant="outlined"
-            required
-            size="small" />
+            size="small"
+            gutterBottom="true"
+          />
         </Grid>
-      </form>
-      <Button 
-        variant="contained">
-          Login
+      <Button
+        alignItems="center" 
+        variant="contained"
+      >
+        Login
       </Button>
+      </form>
     </Grid>
   )
 }
