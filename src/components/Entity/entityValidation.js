8edf9afbece5c127ec2cc285d6b21a272d565d67
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 14:49:12 -0800

    add no specicl character to yup validation

--- src/components/Entity/entityValidation.js
+++ src/components/Entity/entityValidation.js
@@ -4,7 +4,7 @@ const entityValidation = yup.object().shape({
   name: yup.string().required('Company Name is required')
                     .min(3, 'Must be greater than 3 characters')
                     .max(40, 'Must be less than 40 characters')
-                    .matches(/^[aA-zZ\s]+$/, 'No special characters allowed'),
+                    .matches(/^[aA-zZ0-9\s]+$/, 'No special characters allowed'),
 });
 
 export { entityValidation } 
