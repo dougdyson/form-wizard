8bc243f1163e823c6f852384f4ce1df70dc44346
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 14:24:10 -0800

    refactor entity validation

--- src/components/Entity/entityValidation.js
+++ src/components/Entity/entityValidation.js
@@ -2,12 +2,9 @@ import * as yup from "yup";
 
 const entityValidation = yup.object().shape({
   name: yup.string().required('Company Name is required')
-                    .matches(/^[aA-zZ\s]+$/, 'No special characters allowed')
+                    .matches(/^[A-Za-z0-9]*$/, 'No special characters allowed')
                     .min(3, 'Must be greater than 3 characters')
                     .max(40, 'Must be less than 40 characters'),
-  address: yup.string().required('Company wallet address')
-                       .matches(/^[aA-zZ\s]+$/, 'No special characters allowed')
-                       .length(64,'Must be 64 characters long'),
-});
+  });
 
 export { entityValidation } 
