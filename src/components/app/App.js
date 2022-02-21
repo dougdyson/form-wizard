902b5c51f5c321745003a31b86c34ceb7b6d96d1
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 05:13:49 -0800

    remove unused code

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container, Grid } from '@mui/material/';
-import "./app.css"
 import Entity from '../Entity/Entity';
 
 function App() {
