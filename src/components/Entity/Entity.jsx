8235c2dc3767c4e0272c0b5dafb538b74ee827c2
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 14:47:00 -0800

    broke display - stepping back

--- src/components/Entity/Entity.jsx
+++ src/components/Entity/Entity.jsx
@@ -9,7 +9,7 @@ import { entityValidation } from './entityValidation';
 
 function Entity(props){
 
-  const [entity, setEntity] = useState({name: "", address: "04044178226a3132ac6b5c441d839d6cf69d95a8d0d1e0f6eba43498a28a8ea58c8ef889c0ccedc94f5f6467e4caab1776a6867af143b9ba6171b27e6fe96174d1s"})
+  const [entity, setEntity] = useState({name: ""})
   const [error, setError] = useState(null);
   const [helperText, setHelperText] = useState(' ');
 
