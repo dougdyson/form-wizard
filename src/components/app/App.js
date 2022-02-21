f9a04171f69c6f06d202c54bd3acf29bdab58e3c
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 05:03:33 -0800

    remove header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,16 +2,11 @@ import React from 'react';
 // import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Container, Grid } from '@mui/material/';
 import "./app.css"
-
-import Header from '../Header/Header';
 import Entity from '../Entity/Entity';
 
 function App() {
   return (
     <Container>
-      <Grid>
-        <Header />
-      </Grid>
       <Grid>
         <Entity />
       </Grid>
