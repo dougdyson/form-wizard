859cf466901b063d27a17532d66269067b331b7a
Unfamous Dyson <dougdyson@gmail.com>
2022-02-21 14:29:02 -0800

    add address prop to entity object

--- src/components/Entity/entityValidation.js
+++ src/components/Entity/entityValidation.js
@@ -1,10 +1,14 @@
+import { yupToFormErrors } from "formik";
 import * as yup from "yup";
 
 const entityValidation = yup.object().shape({
-  name: yup.string().required('Company Name is required')
-                    .matches(/^[A-Za-z0-9]*$/, 'No special characters allowed')
-                    .min(3, 'Must be greater than 3 characters')
-                    .max(40, 'Must be less than 40 characters'),
+    name: yup.string().required('Company Name is required')
+                      .matches(/^[A-Za-z0-9]*$/, 'No special characters allowed')
+                      .min(3, 'Must be greater than 3 characters')
+                      .max(40, 'Must be less than 40 characters'),
+    address: yupToFormErrors.string().required('Wallet address required')
+                      .length(64)
+                      .matches(/^[A-Za-z0-9]*$/, 'No special characters allowed'),
   });
 
 export { entityValidation } 
