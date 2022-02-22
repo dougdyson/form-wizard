2f85c76e68a1ca6257e8b5243b048fac89053569
Unfamous Dyson <dougdyson@gmail.com>
2022-02-22 15:49:03 -0800

    fix file naming issue

---
+++ src/components/app/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Container, Stack, Grid } from '@mui/material/';
+
+import AppBar from '@mui/material/AppBar';
+import EntityList from '../EntityList/EntityList';
+
+
+function App() {
+
+  const owner = '04044178226a3132ac6b5c441d839d6cf69d95a8d0d1e0f6eba43498a28a8ea58c8ef889c0ccedc94f5f6467e4caab1776a6867af143b9ba6171b27e6fe96174d1';
+
+  return (
+    <Container>
+      <Grid>
+        <AppBar />
+      </Grid>
+      <Grid>
+        <EntityList owner={owner} />
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;
