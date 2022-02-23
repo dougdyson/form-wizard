6e02216264bf902648a2469a5bd6102a5b8d2b34
Unfamous Dyson <dougdyson@gmail.com>
2022-02-22 16:18:21 -0800

    comment out uniqueness check

--- src/data/data.js
+++ src/data/data.js
@@ -42,12 +42,8 @@ const getEntitiesForOwner = (owner) => {
 
 const isUniqueEntityName = (entityName) => {
 
-  console.log('DATA - iUEN - allEntities', allEntities);
-  console.log('DATA - iUEN - entityName', entityName);
   const duplicate = allEntities.find(entity => entity.name === entityName)
 
-  console.log('DATA - iUEN - duplicate', duplicate );
-  
   // if (duplicate.name.length) 
   // {
   //   return false
