73f1aeb23ae20d0e12cd97204f302dc2e5124503
Unfamous Dyson <dougdyson@gmail.com>
2022-02-23 15:22:21 -0800

    checkpoint commit

---
+++ src/components/ComplianceCard/ComplianceCard.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useState } from "react";
+
+import { Grid, Card, CardContent, Typography } from "@mui/material";
+
+import { getComplianceByEntityID } from "../../data/data"
+// need getRuleNameByID
+
+export default function ComplianceCard(props) {
+
+  return(
+    <Grid>
+      <Card>
+        <CardContent>
+          <Typography></Typography>
+        </CardContent>
+      </Card>
+    </Grid>
+  )
+
+}
