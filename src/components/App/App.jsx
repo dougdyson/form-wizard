5c30ec4e172e2c688793faf43e6eaffab797180f
Unfamous Dyson <dougdyson@gmail.com>
2022-02-24 05:34:09 -0800

    cleanup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Container, Grid } from '@mui/material/';
-import AppBar from '../AppBar/AppBar';
 
+import { Container, Grid } from '@mui/material/';
+
+import AppBar from '../AppBar/AppBar';
 import EntityList from '../EntityList/EntityList';
 
 function App() {
 
+  // hardcoded for testing
   const owner = '04044178226a3132ac6b5c441d839d6cf69d95a8d0d1e0f6eba43498a28a8ea58c8ef889c0ccedc94f5f6467e4caab1776a6867af143b9ba6171b27e6fe96174d1';
 
   return (
@@ -13,9 +15,9 @@ function App() {
       <Grid>
         <AppBar />
       </Grid>
-      <Grid>
-        <EntityList owner={owner} />
-      </Grid>
+        <Grid>
+          <EntityList owner={owner} />
+        </Grid>
     </Container>
   );
 }
