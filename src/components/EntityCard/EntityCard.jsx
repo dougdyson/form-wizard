28a6b1fb698b4d628e22cc5de774e0004a79d6c7
Unfamous Dyson <dougdyson@gmail.com>
2022-02-24 05:22:42 -0800

    restore styling

--- src/components/EntityCard/EntityCard.jsx
+++ src/components/EntityCard/EntityCard.jsx
@@ -9,23 +9,37 @@ function EntityCard(props) {
   const entity = getEntityDetailsByID(props.id)
   const jurisdiction = getJurisdictionByID(entity.jurisdictionID)
 
-  const style = {
+  const cardStyle = {
     bgcolor: 'background.paper',
     border: '1px solid #a9a9a9',
     mt: 1,
     mb: 2,
   }
 
+  const jurisdictionStyle = {
+    fontSize: 14, 
+    ml: 1, 
+    mr: 1, 
+    color: "darkgrey"
+  }
+
+  const addressStyle = {
+    fontSize: 14, 
+    ml: 1, 
+    mr: 1, 
+    color: "darkgrey",
+  }
+
   return (
 
     <Grid>
-      <Card sx={style}>
+      <Card sx={cardStyle}>
         <CardActionArea>
           <CardContent>
             <Grid>
               <Typography sx={{ml: 1, mr: 1}}>{entity.name}, LLC</Typography>
-              <Typography variant="overline" sx={{ml: 1, mr: 1}}>{jurisdiction.name}</Typography>
-              <Typography sx={{fontSize: 14, ml: 1, mr: 1, color: "darkgrey"}} noWrap>{entity.address}</Typography>
+              <Typography variant="overline" sx={jurisdictionStyle}>{jurisdiction.name}</Typography>
+              <Typography sx={addressStyle} noWrap>{entity.address}</Typography>
             </Grid>
           </CardContent>
         </CardActionArea>
